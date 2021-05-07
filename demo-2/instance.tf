3df05bf287980dfebc4d6c699711b1fd6c1a7d34
Sergio Márquez <smarquezs@gmail.com>
2021-05-06 22:18:14 -0400

    Add output to print instance public ip.

--- demo-2/instance.tf
+++ demo-2/instance.tf
@@ -3,7 +3,7 @@ resource "aws_key_pair" "mykey" {
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
-resource "aws_instance" "example2" {
+resource "aws_instance" "example" {
   ami = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
   key_name = aws_key_pair.mykey.key_name
@@ -19,11 +19,18 @@ resource "aws_instance" "example2" {
       "sudo /tmp/script.sh",
     ]
   }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
+  }
   connection {
     host        = self.public_ip
     type        = "ssh"
     user        = var.INSTANCE_USERNAME
     private_key = file(var.PATH_TO_PRIVATE_KEY)
   }
+}
 
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
